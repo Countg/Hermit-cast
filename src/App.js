4fed5773a7b1a4499d731ea768caaa037e5c9e0b
Gavin Stephens <gstephens090@gmail.com>
2022-02-08 11:43:47 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+  coldQuotes,
+  hotQuotes,
+  snowQuotes,
+  rainQuotes,
+  sunnyQuotes,
+} from './lib/quotes';
+
+import {
+  BsFillCloudSnowFill,
+  BsCloudRainFill,
+  BsFillSunFill,
+  BsCloudsFill,
+} from 'react-icons/bs';
+
+import './App.css';
+
+const api = {
+  key: process.env.REACT_APP_WEATHER_API,
+  base: 'http://api.openweathermap.org/data/2.5/',
+};
+
+function App() {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [data, setData] = useState([]);
+
+  // random quotes
+
+  let tempQuotes = [];
+  const hotInx = Math.floor(Math.random() * hotQuotes.length);
+  const coldIndx = Math.floor(Math.random() * coldQuotes.length);
+  const snowIndx = Math.floor(Math.random() * snowQuotes.length);
+  const rainIndx = Math.floor(Math.random() * rainQuotes.length);
+  const sunIndx = Math.floor(Math.random() * sunnyQuotes.length);
+
+  const cold = coldQuotes[coldIndx].text;
+  const hot = hotQuotes[hotInx].text;
+  const sun = sunnyQuotes[sunIndx].text;
+  const rain = rainQuotes[rainIndx].text;
+  const snow = snowQuotes[snowIndx].text;
+
+  if (typeof data.main === 'undefined') {
+    tempQuotes.push('');
+  } else if (
+    data.weather[0].main === 'Clear' ||
+    (data.weather[0].main === 'Clouds' && data.main.temp >= 5)
+  ) {
+    tempQuotes.push(hot);
+  } else if (
+    data.weather[0].main === 'Clear' ||
+    (data.weather[0].main === 'Clouds' && data.main.temp < 5)
+  ) {
+    tempQuotes.push(cold);
+  } else if (data.weather[0].main === 'Sunny') {
+    tempQuotes.push(sun);
+  } else if (data.weather[0].main === 'Rain') {
+    tempQuotes.push(rain);
+  } else if (data.weather[0].main === 'Snow') {
+    tempQuotes.push(snow);
+  }
+
+  // Icon change
+  let weatherIcon = [];
+  if (typeof data.main === 'undefined') {
+    weatherIcon.push('');
+  } else if (data.weather[0].main === 'Rain') {
+    weatherIcon.push(<BsCloudRainFill size={40} color='#09f7ea' />);
+  } else if (data.weather[0].main === 'Clouds') {
+    weatherIcon.push(<BsCloudsFill size={40} color='#dd219e' />);
+  } else if (data.weather[0].main === 'Snow') {
+    weatherIcon.push(<BsFillCloudSnowFill size={40} color='#FF5733' />);
+  } else if (
+    data.weather[0].main === 'Sunny' ||
+    data.weather[0].main === 'Clear'
+  ) {
+    weatherIcon.push(<BsFillSunFill size={40} color='#f9ff33' />);
+  }
+
+  // location and weather api fetch
+  function isLocation() {
+    return new Promise((res) => {
+      setTimeout(function () {
+        if ('geolocation' in navigator) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          });
+        } else {
+          console.log('Not Available');
+        }
+
+        res();
+      }, 200);
+    });
+  }
+
+  function isWeather() {
+    axios
+      .get(
+        `${api.base}weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=${api.key}`
+      )
+      .then((res) => {
+        setData(res.data);
+      });
+  }
+
+  useEffect(() => {
+    async function finalForm() {
+      await isLocation();
+      isWeather();
+    }
+    finalForm();
+  }, [latitude, longitude]);
+
+  console.log(data);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>HERMIT-CAST</h1>
+        {typeof data.main == 'undefined' ? (
+          <h1>Waiting for weather in your area</h1>
+        ) : (
+          <div className='temp-info'>
+            <h2>
+              The current temperature in {data.name} is{' '}
+              {Math.round(data.main.temp)}˚ {weatherIcon}
+            </h2>
+            <h6>Why you should stay home:</h6>
+            <p>{tempQuotes}</p>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/quotes.js
@@ -0,0 +1,119 @@
+export const coldQuotes = [
+  {
+    text: 'You already have a freezer at home, why go out and do freezer every where else?',
+  },
+  {
+    text: 'Your bed never gave you frost bite',
+  },
+  {
+    text: 'Why deal with the cold, when House Hunters International is ALWAYS on?',
+  },
+  {
+    text: 'I would rather quit my job, go off the grid, and drink my Bubly from dirty mason jars,  instead of dealing with this cold',
+  },
+  {
+    text: "This winter B.S happens every year. It's time to take a stand... While sitting on the couch... For freedom!",
+  },
+  {
+    text: 'If you go outside in this cold,  your risk of having to do what those rugby players did in that movie, raises exponentially',
+  },
+  {
+    text: "Do you even know what's on HGTV right now?",
+  },
+  {
+    text: "There has to be some sort of leftist reason not to go out in this cold. Give me time. I'll find it",
+  },
+  {
+    text: 'Do you have fur? No? I rest my case',
+  },
+  {
+    text: "You wouldn't have to be going to work in this cold. if you only had taken the time to figure out how to monetize your toe nail clippings",
+  },
+];
+
+export const hotQuotes = [
+  {
+    text: 'Your bed never gave you a sunburn',
+  },
+  {
+    text: "Sure it's 'nice out'  but only sheeple like 'nice out'. Are you a sheeple?",
+  },
+  {
+    text: 'Dude you have an oven,  why go out where over is EVERYWHERE?',
+  },
+  {
+    text: "You're going to the beach? Ok but first let me tell you about this documentary I watched, called Jaws",
+  },
+  {
+    text: 'Why on gods green earth would you want to sweat?',
+  },
+  {
+    text: "There's two kinds of people in life. Those that like warm weather activities, and people who aren't psychopaths.",
+  },
+  {
+    text: "Do you even know what's on HGTV right now?",
+  },
+  {
+    text: "If you step foot outside that door, I'm calling your Mom and telling her you don't eat vegetables.",
+  },
+  {
+    text: "Let's agree to disagree, and agree I'm right about it being too hot for you to go outside",
+  },
+  {
+    text: "You wouldn't have to be going out in this heat, if you just had taken the time to figure out how to monetize your toe nail clippings",
+  },
+];
+
+export const sunnyQuotes = [
+  {
+    text: "It's really sunny out. Studies show that the strongest SPF is your TV room",
+  },
+  {
+    text: "Look at that sun.Trust me, you don't want any piece of that",
+  },
+  {
+    text: 'People see the Sun and they want to go outside and enjoy it. People are also stupid',
+  },
+  {
+    text: 'Nobody talks about the positive sides of lacking vitamin D',
+  },
+  {
+    text: "You know you can still wear your aviators at home? It's not just for shirtless beach volleyball anymore",
+  },
+];
+
+export const rainQuotes = [
+  {
+    text: 'The problem is everyone romanticizes puddles. The reality is sometimes they can hide real dangers like, broken glass...or al-Qaeda ',
+  },
+  {
+    text: "If you need to be out in the rain so badly, just take a bath in your toilet. It's basically the same thing",
+  },
+  {
+    text: "Look it's raining, isn't time to get reacquainted with HOUSE HUNTERS INTERNATIONAL?",
+  },
+  {
+    text: "Fine, I'll just come out and say it. If you go out in this rain, you'll miss Drake. Because I invited him to your home. I was saving this for your birthday, but you forced my hand",
+  },
+  {
+    text: "Go outside, but how do you know someone didn't pee in your rain boots?",
+  },
+];
+
+export const snowQuotes = [
+  {
+    text: 'I heard the snow is up to your knees out there. On wrong step, and that could be your NBA career!',
+  },
+  {
+    text: "It's snowing. You know if you step outside, there's a chance an old lady will ask you to shovel her driveway.",
+  },
+  {
+    text: "Wasn't it the ice age that killed the Dinosaurs? If you want to take that risk, be my guest and go outside.",
+  },
+  {
+    text: 'You know what I love about a snow day like today? My duvet',
+  },
+  {
+    text: "I don't want to bring up Godwins law, but you know what they call people who like going out in the snow, right?",
+  },
+];
